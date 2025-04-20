7c31ea1d81079651699bcf5bfa9c32da98e4ffa9
samuel-eric <samueltan086@gmail.com>
2025-04-20 18:29:15 +0700

    add block to block type function

--- src/block.py
+++ src/block.py
@@ -1,3 +1,45 @@
+from enum import Enum
+import re
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
 def markdown_to_blocks(markdown):
     lines = list(filter(lambda line: line != "",map(lambda line: line.strip(),markdown.split("\n\n"))))
     return lines
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+    block_type = ""
+    for i in range(0, len(lines)):
+        if re.search(r"\#{1,6} .*", lines[i]):
+            block_type = BlockType.HEADING
+        elif re.search(r"\`\`\`\n|.*?\`\`\`", lines[i]):
+            block_type = BlockType.CODE
+        elif re.search(r"\>.*", lines[i]):
+            if block_type != "" and block_type != BlockType.QUOTE:
+                block_type = BlockType.PARAGRAPH
+            else:
+                block_type = BlockType.QUOTE
+        elif re.search(r"\- .*", lines[i]):
+            if block_type != "" and block_type != BlockType.UNORDERED_LIST:
+                block_type = BlockType.PARAGRAPH
+            else:
+                block_type = BlockType.UNORDERED_LIST
+        elif re.search(r"\d+\. .*", lines[i]):
+            if block_type != "" and block_type != BlockType.ORDERED_LIST:
+                block_type = BlockType.PARAGRAPH
+            else:
+                number = int(re.findall(r"(\d+)\. .*", lines[i])[0]) - 1
+                if number == i:
+                    block_type = BlockType.ORDERED_LIST
+                else:
+                    block_type = BlockType.PARAGRAPH
+        else:
+            block_type = BlockType.PARAGRAPH
+    return block_type
--- src/test_block.py
+++ src/test_block.py
@@ -3,7 +3,7 @@ import unittest
 from block import *
 
 class TestBlock(unittest.TestCase):
-    def test_markdown_to_blocks(self):
+    def test_markdown_to_blocks_1(self):
         md = """
 This is **bolded** paragraph
 
@@ -23,7 +23,7 @@ This is the same paragraph on a new line
             ],
         )
 
-    def test_markdown_to_blocks(self):
+    def test_markdown_to_blocks_2(self):
         md = """
 This is **bolded** paragraph
 
@@ -45,6 +45,54 @@ This is the new paragraph on a new line
                 "- This is a list",
                 "- This is another list"
             ],
+        )
+
+    def test_block_to_block_type_unordered_list(self):
+        md = "- This is a list\n- This is another list"
+        block_type = block_to_block_type(md)
+        self.assertEqual(
+            block_type,
+            BlockType.UNORDERED_LIST
+        )
+
+    def test_block_to_block_type_heading(self):
+        md = "#### heading 4"
+        block_type = block_to_block_type(md)
+        self.assertEqual(
+            block_type,
+            BlockType.HEADING
+        )
+
+    def test_block_to_block_type_paragraph(self):
+        md = ">this is quote\n- this is list"
+        block_type = block_to_block_type(md)
+        self.assertEqual(
+            block_type,
+            BlockType.PARAGRAPH
+        )
+
+    def test_block_to_block_type_code(self):
+        md = "```this is code```"
+        block_type = block_to_block_type(md)
+        self.assertEqual(
+            block_type,
+            BlockType.CODE
+        )
+
+    def test_block_to_block_type_ordered_list_1(self):
+        md = "1. this is quote\n2. this is list"
+        block_type = block_to_block_type(md)
+        self.assertEqual(
+            block_type,
+            BlockType.ORDERED_LIST
+        )
+
+    def test_block_to_block_type_ordered_list_2(self):
+        md = "1. this is quote\n2.this is list"
+        block_type = block_to_block_type(md)
+        self.assertEqual(
+            block_type,
+            BlockType.PARAGRAPH
         )
 
 if __name__ == "__main__":
