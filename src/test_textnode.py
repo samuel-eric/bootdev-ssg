a6f773213cd175cf11fe1f539403fa6dc7e8570a
samuel-eric <samueltan086@gmail.com>
2025-04-20 12:53:30 +0700

    add split nodes function

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import *
 
 
 class TestTextNode(unittest.TestCase):
@@ -22,6 +22,44 @@ class TestTextNode(unittest.TestCase):
         node3 = TextNode("This is a link node", TextType.LINK)
         self.assertNotEqual(node, node2)
         self.assertNotEqual(node, node3)
+
+    def test_split_delimiter_code(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        expected_result = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+        ]
+        self.assertEqual(new_nodes, expected_result)
+
+    def test_split_delimiter_bold(self):
+        node1 = TextNode("This is text with a **bold** word", TextType.TEXT)
+        node2 = TextNode("This is text with a `code block` word", TextType.TEXT)
+        node3 = TextNode("This is also a text with a **bold** word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node1, node2, node3], "**", TextType.BOLD)
+        expected_result = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" word", TextType.TEXT),
+            TextNode("This is text with a `code block` word", TextType.TEXT),
+            TextNode("This is also a text with a ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" word", TextType.TEXT),
+        ]
+        self.assertEqual(new_nodes, expected_result)
+
+    def test_split_delimiter_not_text(self):
+        node1 = TextNode("This is text with a **bold** word", TextType.BOLD)
+        node2 = TextNode("This is also a text with a **bold** word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node1, node2], "**", TextType.BOLD)
+        expected_result = [
+            TextNode("This is text with a **bold** word", TextType.BOLD),
+            TextNode("This is also a text with a ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" word", TextType.TEXT),
+        ]
+        self.assertEqual(new_nodes, expected_result)
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -19,3 +19,21 @@ class TextNode():
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT or old_node.text.count(delimiter) == 0:
+            new_nodes.append(old_node)
+        else:
+            if old_node.text.count(delimiter) == 2:
+                split_texts = old_node.text.split(delimiter)
+                for i in range(0, len(split_texts)):
+                    if i == 1:
+                        new_node = TextNode(split_texts[i], text_type)
+                    else:
+                        new_node = TextNode(split_texts[i], TextType.TEXT)
+                    new_nodes.append(new_node)
+            if old_node.text.count(delimiter) == 1:
+                raise Exception("invalid markdown syntax")
+    return new_nodes
